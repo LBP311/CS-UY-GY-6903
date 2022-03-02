8e1d1dcd8f4e458e38adf5c08d88e0dbb1bacd10
Luis Pizarro <lbp311@nyu.edu>
2022-03-01 19:47:05 -0500

    Added Project Notes and "possible" Implementation Notes

--- Decryptor.cpp
+++ Decryptor.cpp
@@ -4,3 +4,31 @@
 #include <vector>
 using namespace std;
 
+/*
+Project Notes:
+1) Both tests contain 500 characters
+2) Space will be found between each word
+
+Implementation Thinking:
+1) Common letters in English: a, e, h, i, n, o, r, s, t	(9 Letters Total)
+2) Begin letters Commonly found: a, d, o, t, w	(5 Letters Total)
+3) End letters Commonly found: d, e, s, t	(4 Letters Total)
+	For more information and reference: https://www.thiagi.com/instructional-puzzles-original/2015/2/13/cryptograms#:~:text=Letter%20Frequency,s%2C%20d%2C%20and%20t.
+
+4) List of 58,000+ English Words (Dictionary_Words.txt)
+	http://www.mieliestronk.com/corncob_lowercase.txt
+5) Dictionary 1 Words
+	As Provided (Dict1.txt)
+		https://brightspace.nyu.edu/content/enforced/176739-945-1224_SP22CS-GYCS-UY47836903AIINET/dictionary_1.txt
+	Removed Unnecessary Headers and Spaces (Dict1_Revised.txt)
+6) Dictionary 2 Words
+	As Provided (Dict2.txt)
+		https://brightspace.nyu.edu/content/enforced/176739-945-1224_SP22CS-GYCS-UY47836903AIINET/dictionary_2.txt
+	Removed Unnecessary Headers and Spaces (Dict2_Revised.txt)
+*/
+
+int main() {
+
+
+	return 0;
+}
