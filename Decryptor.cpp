fb85c624ac3e37a87b5ace02767aa566d075ace0
Luis Pizarro <lbp311@nyu.edu>
2022-03-01 21:35:09 -0500

    Added a question portion to Notes.

--- Decryptor.cpp
+++ Decryptor.cpp
@@ -1,35 +1,154 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-using namespace std;
+#include
+<iostream>
+#include
+<fstream>
+#include
+<string>
+#include
+<vector>
+using
+namespace
+std;
 
 /*
-Project Notes:
-1) Both tests contain 500 characters
-2) Space will be found between each word
+Project
+Notes:
+1)
+Both
+tests
+contain
+500
+characters
+2)
+Space
+will
+be
+found
+between
+each
+word
 
-Implementation References:
-1) Common letters in English: a, e, h, i, n, o, r, s, t	(9 Letters Total)
-2) Begin letters Commonly found: a, d, o, t, w	(5 Letters Total)
-3) End letters Commonly found: d, e, s, t	(4 Letters Total)
-*For more information and reference: https://www.thiagi.com/instructional-puzzles-original/2015/2/13/cryptograms#:~:text=Letter%20Frequency,s%2C%20d%2C%20and%20t.
-*Better Reference: https://norvig.com/mayzner.html
+Implementation
+References:
+1)
+Common
+letters
+in
+English:
+a,
+e,
+h,
+i,
+n,
+o,
+r,
+s,
+t	(9
+Letters
+Total)
+2)
+Begin
+letters
+Commonly
+found:
+a,
+d,
+o,
+t,
+w	(5
+Letters
+Total)
+3)
+End
+letters
+Commonly
+found:
+d,
+e,
+s,
+t	(4
+Letters
+Total)
+*For
+more
+information
+and
+reference:
+https://www.thiagi.com/instructional-puzzles-original/2015/2/13/cryptograms#:~:text=Letter%20Frequency,s%2C%20d%2C%20and%20t.
+*Better
+Reference:
+https://norvig.com/mayzner.html
 
-4) List of 58,000+ English Words (Dictionary_Words.txt)
+4)
+List
+of
+58,000+
+English
+Words
+(Dictionary_Words.txt)
 	http://www.mieliestronk.com/corncob_lowercase.txt
-5) Dictionary 1 Words
-	As Provided (Dict1.txt)
+5)
+Dictionary
+1
+Words
+	As
+Provided
+(Dict1.txt)
 		https://brightspace.nyu.edu/content/enforced/176739-945-1224_SP22CS-GYCS-UY47836903AIINET/dictionary_1.txt
-	Removed Unnecessary Headers and Spaces (Dict1_Revised.txt)
-6) Dictionary 2 Words
-	As Provided (Dict2.txt)
+	Removed
+Unnecessary
+Headers
+and
+Spaces
+(Dict1_Revised.txt)
+6)
+Dictionary
+2
+Words
+	As
+Provided
+(Dict2.txt)
 		https://brightspace.nyu.edu/content/enforced/176739-945-1224_SP22CS-GYCS-UY47836903AIINET/dictionary_2.txt
-	Removed Unnecessary Headers and Spaces (Dict2_Revised.txt)
+	Removed
+Unnecessary
+Headers
+and
+Spaces
+(Dict2_Revised.txt)
+
+Questions to Ask:
+1) Does a new-line character (\n) equate to a space (" ")?
+*/
+
+int
+main()
+{
+Lets
+us
+know
+whether
+or
+not
+we
+completed
+the
+Decryption
+	bool
+complete
+=
+false;
+	//
+Reads
+File
+	ifstream
+encoded;
+	//
+Holds
+Encrypted
+Lines
+	vector<string>
 
 
-int main() {
-
-
-	return 0;
+	return
+0;
 }
